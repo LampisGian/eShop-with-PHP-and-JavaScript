322c3b446ff02cd3cb72ca0311a1bf34bf988466
Lampis Giannelis <labischr@gmail.com>
2026-04-30 17:48:24 +0100

    gitignore

--- Source_Code/config/mail.php
+++ Source_Code/config/mail.php
@@ -9,7 +9,7 @@ return [
     'host' => 'smtp-relay.brevo.com',
     'port' => 587,
     'username' => 'a9a7f7001@smtp-brevo.com',
-    'password' => 'xsmtpsib-b50fb2b25b562d207f2c6060b5f7716b961678345fd1fc351d12a231a530881c-eta94mr90sDEb2yb',
+    'password' => 'xsmtpsib-b50fb2b25b562d207f2c6060b5f7716b961678345fd1fc351d12a231a530881c-bDBIjkrhu1CL4oio',
     'from_email' => 'labischr@gmail.com',
     'from_name' => 'eShop Project'
 ];
